c4c5a874412e5fabc081e00d6022d99103b68274
Jhon Tafur <jhontamos@hotmail.com>
2026-07-18 12:14:37 -0500

    second commit

---
+++ src/main/java/com/jet/align/AlignApplication.java
@@ -0,0 +1,13 @@
+package com.jet.align;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jet/align/AlignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jet.align;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
